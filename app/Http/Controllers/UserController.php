cba4b8b384831dd4707405786845d51e498ada98
Catarina <cscatrisa23@gmail.com>
2018-05-22 16:04:42 +0100

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth','admin'])->only('list', 'blockUser', 'promoteUser', 'unblockUser', 'demoteUser');
+        $this->middleware('admin')->only('index', 'blockUser', 'promoteUser', 'unblockUser', 'demoteUser');
     }
 
     public function list(Request $request){
@@ -21,7 +21,7 @@ class UserController extends Controller
     public function listFilter(Request $request)
     {
         //Somente o campo nome preenchido
-        if ($request->filled('name') && !$request->filled('type', 'status'))
+        if ($request->filled('name') && !$request->filled('type') && !$request->filled('status'))
             return User::where('name', 'like', "%{$request->query('name')}%")->get();
 
         //Somente o tipo preenchido
@@ -33,22 +33,74 @@ class UserController extends Controller
         }
 
         //Somente o status preenchido
-        if ($request->filled('status') && !$request->filled('name') && !$request->filled('name')){
+        if ($request->filled('status') && !$request->filled('type') && !$request->filled('name')){
             if ($request->query('status') == "blocked")
-                return User::where('admin', 1)->get();
+                return User::where('blocked', 1)->get();
             if ($request->query('status') == "unblocked")
-                return User::where('admin', 0)->get();
+                return User::where('blocked', 0)->get();
         }
 
         //Nome e status preenchido
+        if ($request->filled('name') && $request->filled('status') && !$request->filled('type')){
+            if ($request->query('status') == "blocked")
+                return User::where('blocked', 1)->where('name', 'like', "%{$request->query('name')}%")->get();
+            if ($request->query('status') == "unblocked")
+                return User::where('blocked', 0)->where('name', 'like', "%{$request->query('name')}%")->get();
+        }
 
+        //Tipo e status
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='admin' && $request->filled('status')){
+            //admin e blocked
+            if ($request->query('status') == "blocked")
+                return User::where('admin', 1)->where('blocked',1)->get();
+            //admin e unblocked
+            if ($request->query('status') == "unblocked")
+                return User::where('admin', 1)->where('blocked',0)->get();
+        }
+
+        if(!$request->filled('name')&& $request->filled('type') && $request->query('type')=='normal' && $request->filled('status')){
+            //normal e unblocked
+            if ($request->query('status') == "unblocked")
+                return User::where('admin', 0)->where('blocked',0)->get();
+            //normal e blocked
+            if ($request->query('status') == "blocked")
+                return User::where('admin', 0)->where('blocked',1)->get();
+        }
         //Nome e tipo preenchido
-
-        //Status e nome preenchido
-
-        //Tipo e nome preenchido
+        if ($request->filled('name') && !$request->filled('status') && $request->filled('type')){
+            if ($request->query('type') == "admin")
+                return User::where('admin', 1)->where('name', 'like', "%{$request->query('name')}%")->get();
+            if ($request->query('type') == "normal")
+                return User::where('admin', 0)->where('name', 'like', "%{$request->query('name')}%")->get();
+        }
 
         //Todos preenchidos
+        if ($request->filled('name') && $request->filled('status') && $request->filled('type')){
+            //nome + admin + blocked
+            if($request->query('type')=='admin'){
+                if($request->query('status')=='blocked'){
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin', 1)->where('blocked', 1)->get();
+                }
+            }
+            //nome + admin + unblocked
+            if($request->query('type')=='admin'){
+                if($request->query('status')=='unblocked'){
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin', 1)->where('blocked', 0)->get();
+                }
+            }
+            //nome + normal + blocked
+            if($request->query('type')=='normal'){
+                if($request->query('status')=='blocked'){
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin', 0)->where('blocked', 1)->get();
+                }
+            }
+            //nome + normal + unblocked
+            if($request->query('type')=='normal'){
+                if($request->query('status')=='unblocked'){
+                    return User::where('name','like','%'.$request->query('name').'%')->where('admin', 0)->where('blocked', 0)->get();
+                }
+            }
+        }
 
         //Nenhum dos campos preenchidos
         return User::all();
@@ -102,8 +154,8 @@ class UserController extends Controller
 
     public function getAssociate_of(){
         $associates = DB::table('associate_members')->where('associated_user_id', Auth::user()->id)->pluck('main_user_id');
-        $associate_ofUsers= User::find($associates);
-        return view('me.listAssociate_of', compact( 'associate_ofUsers'));
+        $associatesUsers= User::find($associates);
+        return view('me.listAssociates', compact( 'associatesUsers'));
     }
 
     public function changePassword(User $user){
