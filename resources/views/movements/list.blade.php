5e948ae2b421eeacc2f359c3bae801eee5f1a234
goncalourenco <goncalo.blourenc@gmail.com>
2018-05-29 10:56:39 +0100

    US. 20

---
+++ resources/views/movements/list.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        @if(count($movements))
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <td>Account</td>
+                    <td>Type</td>
+                    <td>Category</td>
+                    <td>Date</td>
+                    <td>Value</td>
+                    <td>Start balance</td>
+                    <td>End balance</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($movements as $movement)
+                    <tr>
+                        <td>{{$movement->id}}</td>
+                        <td>{{$movement->type}}</td>
+                        <td>{{$movement->category->name}}</td>
+                        <td>{{$movement->date}}</td>
+                        <td>{{$movement->value}}</td>
+                        <td>{{$movement->start_balance}}</td>
+                        <td>{{$movement->end_balance}}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @else
+            <h2>No movements found</h2>
+    @endif
+@endsection
