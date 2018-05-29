8a8203ef6ab16f241b9eaa26a5c0a361bb0d4e69
goncalourenco <goncalo.blourenc@gmail.com>
2018-05-29 13:06:10 +0100

    US. 20

--- app/Http/Middleware/IsAssociateOf.php
+++ app/Http/Middleware/IsAssociateOf.php
@@ -22,6 +22,7 @@ class isAssociateOf
         if ($request->route('user') && ($request->route('user')->id == Auth::user()->id ||
                 count(DB::table('associate_members')->where('associated_user_id', Auth::user()->id)->where('main_user_id', $request->route('user')->id)->get())>0))
             return $next($request);
+
         $error = "You don't have the permission to see ".$request->route('user')->name."'s movemments!'";
         return Response::make(view('home', compact('error')), 403);
 
